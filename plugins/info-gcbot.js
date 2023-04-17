d59f3d94458b34f4583ca280da12b6bbd87632db
PhmiFz <azrailfanz@gmail.com>
2023-04-17 12:29:29 +0700

    bismillah

---
+++ plugins/info-gcbot.js
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+
+let str = `
+*Klik bacaan "Jᴏɪɴ Sɪɴɪ Cᴜʏ*"
+`
+
+conn.sendButtonDoc(m.chat, str, wm,' ᴏᴋᴇ ','huuu', m, { contextInfo: { externalAdReply: { showAdAttribution: false,
+    mediaUrl: 'https://chat.whatsapp.com/IEV10YxkOz7Kl4qaBgRStS',
+    mediaType: 2, 
+    description: sgc,
+    title: '≪ Jᴏɪɴ Sɪɴɪ Cᴜʏ ⋟',
+    body: wm,
+    thumbnail: await(await fetch("https://telegra.ph/file/90ee8b491168682c27583.jpg")).buffer(),
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['gczyko']
+
+handler.tags = ['info']
+
+handler.command = /^gczyko|gcbot$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.exp = 3
+
+export default handler
